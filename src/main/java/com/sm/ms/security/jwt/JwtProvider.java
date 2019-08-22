8a5648e3046917a03f090a84c843aec8026f3356
anhnt2989 <anhnt2989@gmail.com>
2019-08-22 17:04:43 +0700

    branched dev

--- src/main/java/com/sm/ms/security/jwt/JwtProvider.java
+++ src/main/java/com/sm/ms/security/jwt/JwtProvider.java
@@ -14,10 +14,10 @@ import java.util.Date;
 public class JwtProvider {
     private static final Logger logger = LoggerFactory.getLogger(JwtProvider.class);
 
-    @Value("${grokonez.app.jwtSecret}")
+    @Value("strongmen")
     private String jwtSecret;
 
-    @Value("${grokonez.app.jwtExpiration}")
+    @Value("")
     private int jwtExpiration;
 
     public String generateJwtToken(Authentication authentication) {
