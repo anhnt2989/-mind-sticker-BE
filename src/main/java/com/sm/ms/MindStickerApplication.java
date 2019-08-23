98f3ccec835aa73e8825e8cf6f6678bc16c41e09
anhnt2989 <anhnt2989@gmail.com>
2019-08-23 18:52:25 +0700

    trying to deploy

--- src/main/java/com/sm/ms/MindStickerApplication.java
+++ src/main/java/com/sm/ms/MindStickerApplication.java
@@ -1,11 +1,27 @@
 package com.sm.ms;
 
+import com.sm.ms.service.NoteService;
+import com.sm.ms.service.impl.NoteServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MindStickerApplication {
+public class MindStickerApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(MindStickerApplication.class, args);
     }
+
+    @Bean
+    public NoteService noteService() {
+        return new NoteServiceImpl();
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MindStickerApplication.class);
+    }
+
 }
